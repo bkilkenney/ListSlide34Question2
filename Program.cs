35c605b80522d2c9144368134b114ac357712b36
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-19 16:50:42 -0400

    ListSlide34Question2

--- Program.cs
+++ Program.cs
@@ -10,6 +10,26 @@ namespace ListSlide34Question2
     {
         static void Main(string[] args)
         {
+            List<bool> boolList = new List<bool>() { true, false, false, true, false };
+
+            
+
+            foreach (bool currentVal in boolList)
+                if(currentVal == true)
+                {
+                    Console.WriteLine("Better bring an umbrella");
+                }
+            else if (currentVal == false)
+                {
+                    Console.WriteLine("No rain today, enjoy the sun!");
+                }
+                    
+            
+
+            //Loop through each value
+            // If the value is true print: "Better bring an umbrella"
+            //If the value is false print: "No rain today, enjoy the sun!"
+
         }
     }
 }
